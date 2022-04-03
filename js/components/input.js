2cd4a54f3256285265d1b91d44ff8ddf7c1b541d
DragneilDaedalus <octymer2@gmail.com>
2022-04-04 01:38:05 +0700

    index.html

---
+++ js/components/input.js
@@ -0,0 +1,51 @@
+class InputComponent {
+	$container;
+	$label;
+	$containerInput;
+	$input;
+
+	$error;
+
+	constructor(label, name, id, type) {
+		this.$container = document.createElement('div');
+		this.$container.classList.add('row', 'd-flex', 'flex-wrap', 'mt-4');
+
+		this.$label = document.createElement('label');
+		this.$label.classList.add('col-4');
+		this.$label.innerText = label;
+
+		this.$containerInput = document.createElement('div');
+		this.$containerInput.classList.add('col-8');
+
+		this.$input = document.createElement('input');
+		this.$input.classList.add('form-control');
+		this.$input.type = type;
+		this.$input.id = id;
+		this.$input.name = name;
+
+		this.$error = document.createElement('div');
+		this.$error.classList.add('error', 'mt-2', 'd-none');
+	}
+	setError(message) {
+		this.$error.innerText = message;
+		this.$error.classList.remove('d-none');
+		this.$error.classList.add('d-block');
+	}
+
+	setAttribute(name, value) {
+		this.$input.setAttribute(name, value);
+	}
+
+	setEventListener(event, callbackFunction) {
+		this.$input.addEventListener(event, callbackFunction);
+	}
+
+	render() {
+		this.$container.append(this.$label, this.$containerInput);
+		this.$containerInput.append(this.$input, this.$error);
+
+		return this.$container;
+	}
+}
+
+export default InputComponent;
